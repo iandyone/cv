15a8622d95450d8116a8b33adaa0c729b201dbaa
Andrei Bernatski <iandy1731@gmail.com>
2022-12-05 17:34:34 +0300

    UI fixes

--- js/scroll-anims.js
+++ js/scroll-anims.js
@@ -3,6 +3,6 @@ import LocomotiveScroll from 'locomotive-scroll';
 const scroll = new LocomotiveScroll({
     el: document.querySelector('[data-scroll-container]'),
     smooth: true,
-    multiplier: ".5",
+    multiplier: ".4",
     reloadOnContextChange: true,
 })
