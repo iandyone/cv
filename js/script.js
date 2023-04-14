20f96fd7951a5b732de1726a43745c7896ae8365
Andrei Bernatski <iandy1731@gmail.com>
2023-04-14 15:35:34 +0300

    upd: update background animation, skill section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 import "./locomotive-scroll.min.js"
 import "./scroll-anims";
-import "./snow";
+import 'particles.js'
+// import "./snow";
+
+
+particlesJS.load('particles-js', './particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,3 @@ module.exports = {
     },
     mode: 'production',
 }
-// module.exports = {
-//     entry: './js/scroll-anims',
-//     output: {
-//         path: `${__dirname}/js/`,
-//         filename: 'index.js',
-//     },
-//     mode: 'production',
-// }
